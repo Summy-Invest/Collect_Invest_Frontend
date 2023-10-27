d5b3efa6151a03c2bac3e39606ffb94df623a8f0
originalyusername <tanyapetrop@gmail.com>
2023-10-27 21:19:15 +0300

    products carousel added

--- app/src/main/java/com/example/collectinvest/adapters/ProductCarouselAdapter.kt
+++ app/src/main/java/com/example/collectinvest/adapters/ProductCarouselAdapter.kt
@@ -1,31 +1,40 @@
 package com.example.collectinvest.adapters
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.collectinvest.R
 import com.example.collectinvest.models.ProductModel
 import com.example.collectinvest.ui.item.ItemFragment
+import com.squareup.picasso.Picasso
 
-class ProductCarouselAdapter(fragment: Fragment, private val products: List<ProductModel>) :
-    FragmentStateAdapter(fragment) {
+class ProductCarouselAdapter(private val dataList: List<ProductModel>) : RecyclerView.Adapter<ProductCarouselAdapter.ViewHolder>() {
 
-    override fun getItemCount(): Int {
-        return products.size
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.product_item, parent, false)
+        return ViewHolder(view)
     }
 
-    override fun createFragment(position: Int): Fragment {
-        val product = products[position]
-        val itemFragment = ItemFragment.newInstance()
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = dataList[position % dataList.size] // Используйте остаток от деления, чтобы создать эффект бесконечной карусели
+        holder.carouselText.text = item.description
 
-        // Передача данных о товаре во фрагмент через аргументы
-        val args = Bundle()
-        args.putString("imgUrl", product.imgUrl.toString())
-        args.putString("name", product.name)
-        args.putString("description", product.description)
-        args.putDouble("price", product.price)
-        args.putString("currency", product.currency.currencyCode)
-        itemFragment.arguments = args
+        // Используйте Picasso или другую библиотеку для загрузки изображений
+        Picasso.get().load(item.imgUrl.toString()).into(holder.carouselImage)
+    }
 
-        return itemFragment
+    override fun getItemCount(): Int {
+        return Int.MAX_VALUE // Задайте большое значение для создания впечатления бесконечности
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val carouselImage: ImageView = itemView.findViewById(R.id.carouselImage)
+        val carouselText: TextView = itemView.findViewById(R.id.carouselText)
     }
 }
--- app/src/main/java/com/example/collectinvest/ui/foryou/ForYouFragment.kt
+++ app/src/main/java/com/example/collectinvest/ui/foryou/ForYouFragment.kt
@@ -7,9 +7,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
 import com.example.collectinvest.ItemActivity
@@ -29,6 +32,9 @@ class ForYouFragment : Fragment() {
     var sampleImgs = arrayOf("https://www.funnyart.club/uploads/posts/2023-02/thumbs/1675611210_www-funnyart-club-p-lisa-mem-yumor-20.jpg",
         "https://static.gtri.be/images/4cb4d2/ee3f884dae_m.jpg",
         "https://s00.yaplakal.com/pics/pics_preview/1/2/0/6492021.jpg")
+    var prods = listOf(ProductModel(name = "Lisa1", imgUrl=Url("https://www.funnyart.club/uploads/posts/2023-02/thumbs/1675611210_www-funnyart-club-p-lisa-mem-yumor-20.jpg"), description = "Hype", price = 300.0, currency = Currency.getInstance("USD")),
+        ProductModel(name="Lisa2", imgUrl = Url("https://static.gtri.be/images/4cb4d2/ee3f884dae_m.jpg"), description = "Cringe", price = 200.0, Currency.getInstance("USD")),
+        ProductModel(name="Lisa3", imgUrl = Url("https://s00.yaplakal.com/pics/pics_preview/1/2/0/6492021.jpg"), description = "Rofl", price = 100.0, currency = Currency.getInstance("USD")))
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -45,13 +51,18 @@ class ForYouFragment : Fragment() {
             val intt = Intent(requireContext(), ItemActivity::class.java)
             startActivity(intt)
         }
-        val carouselView: CarouselView = binding.carouselView
+        //val carouselView: CarouselView = binding.carouselView
+        val recyclerView: RecyclerView = binding.carouselRecyclerView
+        val adapter = ProductCarouselAdapter(prods)
+        recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        recyclerView.adapter = adapter
+        //carouselView.setPageCount(sampleImgs.size)
+        //carouselView.setImageListener(imageListener)
 
-        carouselView.setPageCount(sampleImgs.size)
-        carouselView.setImageListener(imageListener)
-        carouselView.stopCarousel()
+        //carouselView.stopCarousel()
         return root
     }
+
     var imageListener: ImageListener = object : ImageListener {
         override fun setImageForPosition(position: Int, imageView: ImageView) {
             // You can use Glide or Picasso here
